e0a690b26d4021773ff580e2d2bf12da43b93895
ayushgitt <ayush795592@gmail.com>
2025-06-19 14:24:29 +0530

    update3

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const video1=document.getElementById("projectvideo1");
 const video2=document.getElementById('projectvideo2');
 const video3=document.getElementById('projectvideo3');
 
+const sidebar= document.querySelector('.sidebar');
+const menu= document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+
 
 const videos=[video1,video2,video3];
 
@@ -14,3 +18,18 @@ videos.forEach(function(video){
         video.currentTime=0;
     })
 })
+
+
+// Toggle sidebar on menu icon click
+menu.addEventListener("click", function() {
+    sidebar.classList.toggle("open-sidebar");
+    console.log("1")
+    console.log(sidebar)
+})
+
+// // Close sidebar on close icon click
+// closeIcon.addEventListener("click", function() {
+//     sidebar.classList.remove("open-sidebar");
+//     console.log("2")
+// })
+
